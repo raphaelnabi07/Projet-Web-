1ebac15a6af0c1f1e6d5f4d7f529f098dffb453c
Raphaël Nabi <raphae.nabi@efrei.net>
2026-03-17 11:26:56 +0100

    Header et Footer finis et .php

---
+++ php/footer.php
@@ -0,0 +1,27 @@
+<footer class="bas-accueil">
+    <div >
+        
+        <div class="bas-accueil-image">
+            <img src="../images/efrei.jpg" width="200" height="75">
+        </div>
+
+        <div >
+            <p><strong>Créer par :</strong> Thierno Diallo & Raphaël Nabi</p>
+            <p><strong>Plus d'info sur :</strong> <a href="apropos.html">A propos</a></p>
+        </div>
+
+        <div>
+            <p><strong>Contact de l'école :</strong></p>
+        </div>
+
+        <div>
+            <p><strong>Bordeaux :</strong> + 33 582 060 162</p>
+            <p><strong>Paris :</strong> + 33 188 289 001</p>
+            <p><strong>Accueil :</strong> + 33 188 289 000</p>
+        </div>
+    </div>
+    
+    <div>
+        <p>Dernière modification : 20/02/2026</p>
+    </div>
+</footer>
---
+++ php/header.php
@@ -0,0 +1,23 @@
+<header class="haut-accueil">
+    <div>
+        <div>
+            <img src="../images/efrei.jpg"  width="200" height="75">
+        </div>
+
+        <nav>
+            <a href="../html/accueil.php"><strong>Accueil</strong></a>
+            <li class="dropdown" id="Menu">
+                <a href="#"><strong>Formations et Cours ▾</strong></a>
+                <div class="sous-menu-content">
+                    <a href="../html/basededonnee.php"><strong>Base de donnée</strong></a>
+                    <a href="../html/langageC.php"><strong>Langage C</strong></a>
+                    <a href="../html/reseau.php"><strong>Réseau</strong></a>
+                    <a href="../html/webdynamique.php"><strong>Web Dynamique</strong></a>
+                </div>
+            </li>
+            <a href="../html/association.php"><strong>Associations</strong></a>
+            <a href="../html/locaux.php"><strong>Locaux</strong></a>
+            <a href="../html/apropos.php"><strong>A propos</strong></a>
+        </nav>
+    </div>
+</header>
