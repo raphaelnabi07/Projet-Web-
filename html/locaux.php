fcce56ebb412c7d08b3e240f256f0c0d94744ada
Thierno Diallo <dia2lo.thierno@gmail.com>
2026-03-17 13:24:46 +0100

    Avancement Locaux.php

--- html/locaux.php
+++ html/locaux.php
@@ -6,7 +6,35 @@
     <title>EFREI Informatique</title>
     <link rel="stylesheet" href="../css/style.css">
     <script src="../js/script.js" defer></script>
+
 </head>
+
+<body> 
 <?php include '../php/header.php'; ?>
+
+<main> 
+    <section class="Locaux Efrei Paris">
+        <h1> Locaux de l'Efrei (Paris)</h1>
+
+        <div class ="La Maison">
+            <h2>La Maison</h2>
+            <img src="Image La Maison" alt="">
+        </div>
+
+        <div class="La Factory">
+            <h2>La Factory</h2>
+            <img src="Image La Factory" alt="">
+        </div>
+
+        <div class="L'Aquarium">
+            <h2>L'Aquarium</h2>
+            <img src="Image L'Aquarium" alt="">
+        </div>
+
+        <div class="New Republic">
+            <h2>New Republic</h2>
+            <img src="Image de New Republic" alt="">
+        </div>
+
 <?php include '../php/footer.php'; ?>
 </html>
