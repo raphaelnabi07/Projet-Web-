a7443ff865c9b15e64748558f54a177cb863b92d
Raphaël Nabi <raphae.nabi@efrei.net>
2026-03-25 19:22:19 +0100

    supprimer et valider permanance

---
+++ php/supprimer.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'db_config.php';
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    //Sécurité passage indirect
+    $stmt = $pdo->prepare("DELETE FROM permanences WHERE id = ?");
+    $stmt->execute([$id]);
+}
+?>
---
+++ php/valider.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'db_config.php';
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    //Securité passage indirect
+    $stmt = $pdo->prepare("UPDATE permanences SET statut = 'Validé' WHERE id = ?");
+    $stmt->execute([$id]);
+    
+}
+?>
